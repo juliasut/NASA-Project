77b1552ca4bf51f7384359db8d75766698bfbcdf
juliasut <juliasutygina@gmail.com>
2023-01-05 11:57:23 -0700

    server awaits planet data load on startup

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,19 @@
 const http = require('http');
 
-const app = require('./app')
+const app = require('./app');
+
+const { loadPlanetsData } = require('./models/planets.model');
 
 PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`)
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
+}
+
+startServer();
