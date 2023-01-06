4e4a761e09f57490218941c2e69609f6ed1f4d44
juliasut <juliasutygina@gmail.com>
2023-01-06 12:36:36 -0700

    posting to launches router

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -9,10 +9,10 @@ async function httpGetPlanets() {
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`);
-  const fetchedLaunches = await response.json()
+  const fetchedLaunches = await response.json();
   return fetchedLaunches.sort((a, b) => {
-    return a.flightNumber - b.flightNumber
-  })
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
 async function httpSubmitLaunch(launch) {
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const { getAllPlanets: httpGetAllPlanets } = require('./planets.controller');
+const { httpGetAllPlanets } = require('./planets.controller');
 
 const planetsRouter = express.Router();
 
-planetsRouter.get('/planets', httpGetAllPlanets);
+planetsRouter.get('/', httpGetAllPlanets);
 
 module.exports = planetsRouter;
