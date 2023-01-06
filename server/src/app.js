ee0396da38a56a779d9d13c4ca97b91064aeef6c
juliasut <juliasutygina@gmail.com>
2023-01-06 11:40:28 -0700

    refactor data access layer

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 app.use(morgan('combined'));
@@ -12,13 +13,14 @@ app.use(
   cors({
     origin: 'http://localhost:3000',
   })
-  );
+);
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
-app.get('/', (req, res) => {
+app.use(launchesRouter);
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,13 +6,17 @@ const launch = {
   rocket: 'Explorer IS1',
   launchDate: new Date('Dec 27, 2030'),
   destination: 'Kepler-442 b',
-  customer: [NASA, ZTM],
+  customer: ['NASA', 'ZTM'],
   upcoming: true,
   success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
 module.exports = {
-  launches,
+  getAllLaunches,
 };
