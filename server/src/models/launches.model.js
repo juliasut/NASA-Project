f94ab9e5627bc60310559e3337bd6eadf990873d
juliasut <juliasutygina@gmail.com>
2023-01-08 21:33:30 -0700

    connect POST /launches with front end dashboard

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: 'Kepler Exp',
   rocket: 'Explorer IS1',
   launchDate: new Date('Dec 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customer: ['NASA', 'ZTM'],
   upcoming: true,
   success: true,
