010e1317e1921b292afbca24b7f00187469ccb32
juliasut <juliasutygina@gmail.com>
2023-01-08 21:01:25 -0700

    validations for POST /launches

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,12 +1,30 @@
 const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
 
-function httpGetAllLaunches(req, res) {
-  return res.status(200).json(getAllLaunches);
+async function httpGetAllLaunches(req, res) {
+  const launches = await getAllLaunches();
+  return res.status(200).json(launches);
 }
 
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
+
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.destination
+  ) {
+    return res.status(400).json({
+      error: 'Missing required launch property',
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: 'Invalid launch date',
+    });
+  }
   addNewLaunch(launch);
 
   return res.status(201).json(launch);
