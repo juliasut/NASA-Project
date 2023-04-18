de0cdfbe8b84582abd210362b9d2449f7e900c16
juliasut <juliasutygina@gmail.com>
2023-04-18 00:06:23 -0600

    check for target planet before saving a launch to keep referential integrity

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
 const launches = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
 let latestFlightNumber = 100;
 
@@ -24,6 +25,14 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planets found');
+  }
+
   await launches.updateOne({ flightNumber: launch.flightNumber }, launch, {
     upsert: true,
   });
